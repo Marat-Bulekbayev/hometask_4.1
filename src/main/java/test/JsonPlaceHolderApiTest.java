256ac6c4b7a1fed9cff3bf333e1b200512f80493
Marat <m.bulekbayev@kazdream.kz>
2021-06-25 17:46:55 +0600

    changes: 1. added json schema validation in verifyHttpResponseBody test

--- src/main/java/test/JsonPlaceHolderApiTest.java
+++ src/main/java/test/JsonPlaceHolderApiTest.java
@@ -1,32 +1,36 @@
 package test;
 
 import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import org.apache.http.HttpStatus;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 import static org.hamcrest.core.Is.is;
 
 public class JsonPlaceHolderApiTest {
 
+    private static final String BASE_PATH = "/users";
+
     @BeforeClass
     public void setup() {
-        RestAssured.baseURI = "https://jsonplaceholder.typicode.com/users";
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
     }
 
     @Test
     public void verifyHttpStatusCode() {
-        given().get().then().assertThat().statusCode(HttpStatus.SC_OK);
+        given().get(BASE_PATH).then().assertThat().statusCode(HttpStatus.SC_OK);
     }
 
     @Test
     public void verifyHttpResponseHeader() {
-        given().get().then().assertThat().contentType("application/json; charset=utf-8");
+        given().get(BASE_PATH).then().assertThat().contentType("application/json; charset=utf-8");
     }
 
     @Test
-    public void verifySizeOfResponseBodyArray() {
-        given().get().then().assertThat().body("size()", is(10));
+    public void verifyHttpResponseBody() {
+        given().get(BASE_PATH).then().assertThat().body(matchesJsonSchemaInClasspath("schemas/users-schema.json")).and().body("size()", is(10));
     }
 }
