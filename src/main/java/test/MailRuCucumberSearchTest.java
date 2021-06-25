c688ec5d59efbdf500df8d4dfdf34fdc6b557d0e
Marat <m.bulekbayev@kazdream.kz>
2021-06-25 16:35:57 +0600

    changes: 1. created MailRuCucumberBaseTest 2. moved AfterClass method in created base test class 3. fixed typo in MailRuSearch.feature

--- src/main/java/test/MailRuCucumberSearchTest.java
+++ src/main/java/test/MailRuCucumberSearchTest.java
@@ -1,20 +1,10 @@
 package test;
 
-import driver.Driver;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
 
 @CucumberOptions(plugin = { "json:target/cucumber-report.json", "html:target/cucumber-report" },
                  features = "src/main/resources/features/MailRuSearch.feature",
                  glue = { "steps" })
-public class MailRuCucumberSearchTest extends AbstractTestNGCucumberTests {
+public class MailRuCucumberSearchTest extends MailRuCucumberBaseTest {
 
-    private static final WebDriver driver = Driver.getDriver();
-
-    @AfterClass
-    public void cleanUp() {
-        driver.quit();
-    }
 }
--- src/main/resources/features/MailRuSearch.feature
+++ src/main/resources/features/MailRuSearch.feature
@@ -2,7 +2,7 @@ Feature: MailRu search test
 Background:
   Given user opens mail.ru main page
 
-Scenario Outline: MailRu login
+Scenario Outline: MailRu search
   When user enters "<search_query>" and press search
   Then first search result contains "<search_query>"
 
